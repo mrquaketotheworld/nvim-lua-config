79a3eef3eeff8514fbe268bac8c8200565c59bcf
mrquaketotheworld <newrushiflyer@gmail.com>
2023-05-31 16:23:24 +0300

    Fix gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,4 +1,12 @@
 require('gitsigns').setup({
+  signs = {
+    add          = { text = '┃' },
+    change       = { text = '∼' },
+    delete       = { text = '┃' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '≃' },
+    untracked    = { text = '┆' },
+  },
   on_attach = function()
     vim.keymap.set('n', '<leader>hp', ':Gitsigns preview_hunk<cr>', { silent = true })
     vim.keymap.set('n', '<leader>hu', ':Gitsigns reset_hunk<cr>', { silent = true })
