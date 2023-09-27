5791c1dff159f5bf443f10ba58e9bf90e295ca12
mrquaketotheworld <newrushiflyer@gmail.com>
2023-09-27 17:33:51 +0300

    Fix lualine & nvim-tree settings

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,9 +1,12 @@
 require('lualine').setup({
   options = {
     theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
   },
   sections = {
+    lualine_b = { { 'branch', icon = '' } },
     lualine_y = {},
     lualine_z = { '%c', '%l/%L' }
-  }
+  },
 })
--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,7 +1,11 @@
 require('nvim-tree').setup({
   view = {
     preserve_window_proportions = true,
-  }
+  },
+  filters = {
+    git_ignored = true,
+    dotfiles = true,
+  },
 })
 
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFile!<cr>', { silent = true })
